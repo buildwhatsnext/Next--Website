766eba8b6d3bdbd8f19cd5bdc040c08d20290546
Preston Smith (HLW) <PrestonII@users.noreply.github.com>
2020-02-27 16:25:33 -0500

    grid is sort of working

--- src/components/home/home.page.js
+++ src/components/home/home.page.js
@@ -8,12 +8,40 @@ export default class Home extends React.Component {
     console.log('In the Home component');
   }
 
+
+
   render() {
+    const options__design = [
+      "design",
+      "architecture",
+      "lighting",
+      "engineering",
+      "sustainability"
+    ]
+
+    const company = "hlw";
+    const designNoun = options__design[0];
+    const tagCompany = `Next is a startup inside of ${company}`;
+    const tagChange = `We're advancing the way technology is used in ${designNoun}`;
+    const cta = 'See What We Do';
+
     return (
-      <div className="start">
-        {/* <p>
-          Home Start!
-        </p> */}
+      <div className="home">
+        <div className="soft__blue__title home__hlw">
+          <p>
+          { tagCompany }
+          </p>
+        </div>
+        <div className="soft__blue__title home__tagline">
+          <p>
+            { tagChange }
+          </p>
+        </div>
+        <div className="soft__blue__subtitle home__cta">
+          <p>
+            { cta }
+          </p>
+        </div>
       </div>
     )
   }
