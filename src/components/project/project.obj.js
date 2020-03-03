0c6950f632852a703f6c438a48165173226e7405
Preston Smith (HLW) <PrestonII@users.noreply.github.com>
2020-03-03 13:34:30 -0500

    added test pieces

--- src/components/project/project.obj.js
+++ src/components/project/project.obj.js
@@ -6,18 +6,69 @@ export default class Project extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: 'We Create Great Work', 
+      projects: [
+        {
+          title: 'Test',
+          type: 'Even Testier'
+        },
+        {
+          title: 'Test2',
+          type: 'Return of the Test'
+        },
+        {
+          title: 'Test3',
+          type: 'The Test Strikes Back'
+        }
+      ]
     }
   }
 
-  render() {
+  populateProjects = () => {
+    console.log('Populating projects');
+    const projects = this.formatProjects(this.state.projects);
+    return projects;
+  }
+
+  formatProjects = (items) => {
+    let i = 0;
+    const listItems = items.map(item => {
+      return <ProjectItem key={i++} {...item} />
+    });
+  
     return (
-      <div className="page page__project">
-        <div className="page__project__title">
-          We Create Great Work
-        </div>
-        <div className="page__project__items"></div>
+      <div className="project__list">
+        { listItems }
       </div>
     )
   }
+
+  render() {
+    const projects = this.populateProjects();
+
+    return (
+      <div className="page page__project">
+        <div className="page__project__title">
+          { this.state.title }
+        </div>
+        <div className="page__project__items">
+          { projects }
+        </div>
+      </div>
+    )
+  }
+}
+
+export function ProjectList(props) {
+  const { items } = props;
+
+}
+
+export function ProjectItem(props) {
+  return (
+    <div className="project__item">
+      <div className="project__item__title">{ props.title } </div>
+      <div className="project__item__type"> { props.type } </div>
+    </div>
+  );
 }
