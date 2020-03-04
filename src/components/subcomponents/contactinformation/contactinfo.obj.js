e3d9013381d5e8d3b43ae3585fb830f809df0cf4
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-03-04 09:59:14 -0500

    all the grids are set

--- src/components/subcomponents/contactinformation/contactinfo.obj.js
+++ src/components/subcomponents/contactinformation/contactinfo.obj.js
@@ -8,10 +8,10 @@ import location from '../../../assets/svg/icons/34location.svg';
 export function ContactInfoList(props) {
   return (
     <div className="contact__info">
-      {/* <Phone />
-      <Email />
-      <Insta />
-      <Location /> */}
+      <Phone />
+      {/* <Email /> */}
+      {/* <Insta /> */}
+      {/* <Location /> */}
     </div>
   )
 }
