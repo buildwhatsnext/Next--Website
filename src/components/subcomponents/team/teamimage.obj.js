ff1df5d1954306c739f211d86878804704fc0db0
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-03-04 19:20:14 -0500

    all the elements are in for desktop screen

--- src/components/subcomponents/team/teamimage.obj.js
+++ src/components/subcomponents/team/teamimage.obj.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Fragment } from 'react';
+import grouppic from '../../../assets/photo/team/group/grouppicture.png'
 
+export class TeamImage extends React.Component {
+    constructor(props) {
+      super(props);
+      }
 
-export function TeamImage(props) {
-    return (
-      <div className="contact__info">
-      </div>
-    )
-  }
+      render() {
+        return (
+          <fragment>
+           <div className="image">
+              <div className="picture"></div>
+              <div className="whitebox"></div>
+              <div className="bluebox"></div>
+            </div>
+          </fragment>
+        )
+      }
+    }
--- src/components/team/team.obj.js
+++ src/components/team/team.obj.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import {TeamText} from '../subcomponents/team/teamtext.obj';
 import {TeamImage} from '../subcomponents/team/teamimage.obj';
+import './team.style.desktop.scss'
 
 export default class Team extends React.Component {
     constructor(props) {
