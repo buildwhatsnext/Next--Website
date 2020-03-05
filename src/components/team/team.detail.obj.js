4f228648636bcc28d869e1200512e970fcba0c9f
Preston Smith (HLW) <PrestonII@users.noreply.github.com>
2020-03-05 14:31:07 -0500

    team members route correctly

--- src/components/team/team.detail.obj.js
+++ src/components/team/team.detail.obj.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import data from '../../data/data.team.json';
 
-
 export default class TeamMemberDetailPage extends React.Component {
   render() {
     const { memberId } = this.props.match.params;
     const info = data.members[memberId];
     const errorQuote = "I'm afraid I can't do that Michael...";
+    console.log(data);
 
-    return info === null 
+    return info === null || info === undefined
       ? <div className="error"><p>{errorQuote}</p></div>
       : <TeamMemberDetail {...info} />
   }
