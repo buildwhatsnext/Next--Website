a55f41a62cf07f62cb2146d7d1a73cc2be9fd44e
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-03-09 09:18:40 -0400

    added link highlight on hover and made it go to correct pages

--- src/components/home/home.page.js
+++ src/components/home/home.page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { LinkHighlighted } from '../subcomponents/special_link/links.object';
+import { Link } from 'react-router-dom';
+import { ExternalLinkHighlighted, InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
 
 import './home.style.desktop.scss';
 import './home.style.mobile.scss';
@@ -25,23 +25,20 @@ export default class Home extends React.Component {
     const tagCompany = `Next is a startup inside of `;
     const tagChange = `We're advancing the way technology is used in `;
     const cta = 'See What We Do';
+    const hlwLink = "https://www.hlw.design/";
 
     return (
       <div className="home">
         <div className="soft__blue__title includes__highlight home__hlw">
-          <p>
-          { tagCompany }
-          </p>
-          <LinkHighlighted value={ company } />
+          <p>{ tagCompany }</p>
+          <ExternalLinkHighlighted value={ company } destination={ hlwLink }/>
         </div>
         <div className="soft__blue__title includes__highlight home__tagline">
           <p>{ tagChange } </p>
           {/* <p>{ designNoun } </p> */}
         </div>
         <div className="soft__blue__subtitle home__cta">
-          <p>
-            { cta }
-          </p>
+          <InternalLinkHighlighted value={cta} destination="/projects" />
         </div>
       </div>
     )
