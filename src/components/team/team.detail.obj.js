f85f4d3fe113e3035adca5d92858d4025272075b
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-03-09 08:19:19 -0400

    just used manually directed the photo location in css for now

--- src/components/team/team.detail.obj.js
+++ src/components/team/team.detail.obj.js
@@ -17,28 +17,15 @@ export default class TeamMemberDetailPage extends React.Component {
 
 
 export function TeamMemberDetail(data) {
-  const url = data.pictureURL;
-
-  console.log(url);
-
+  
   return (
     <div className="team__detail">
-      
       <div className="team__detail__name">{ data.name }</div>
       <div className="team__detail__email">{ data.email }</div>
       <div className="team__detail__position">{ data.position }</div>
       {/* <div className="team__detail__location">{ data.location }</div> */}
       <div className="team__detail__description">{ data.description }</div>
-      {/* <div className="team__detail__team">{ data.team }</div> */}
-      <div 
-        className="team__detail__pictureURL" 
-        style={
-          {
-            backgroundImage: `url('assets/team/professional/kp.PNG')`
-          }
-        }
-      />
-      
+      <div className={`team__detail__pictureURL__${data.name}`} />
       <div className="trapazoid"></div>
       <div className="borderbottom"></div>
       <div className="cta">Get in Touch!</div>
