cf1a41f3c2b62f54d8498c1c2bffcbfca4ad6895
Preston Smith <psmith@HLW.COM>
2020-03-13 07:59:43 -0400

    trying to add themes

--- src/app/app.obj.js
+++ src/app/app.obj.js
@@ -2,10 +2,11 @@ import React from 'react';
 import AppRouter from './app.router';
 import './app.style.desktop.scss';
 import './app.style.mobile.scss';
+import './app.style.general.scss';
 
 export default function App() {
   return (
-    <div className="app">
+    <div className="app app__theme__blue">
       <AppRouter />
     </div>
   )
