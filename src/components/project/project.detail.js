039e8e5dbb481dcf52277bad4b655193af359735
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-03-13 16:29:42 -0400

    project grids are in

--- src/components/project/project.detail.js
+++ src/components/project/project.detail.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import data from '../../data/data.project.json';
+import './project.detail.style.desktop.scss'
 
 export class ProjectDetailPage extends React.Component {
   render() {
@@ -17,8 +18,18 @@ export class ProjectDetailPage extends React.Component {
 
 export function ProjectDetail(data) {
   return (
-    <div className="project__detail">
-      Name: { data.name }
-    </div>
+      <div className="projectdetailpage">
+          <div className="project__title">{ data.title }</div>
+          <div className="project__table">
+            <div className="project__client">Client{ data.client }</div>
+            <div className="project__location">{ data.location }</div>
+            <div className="project__status">{ data.status }</div>
+            <div className="project__type">{ data.type }</div>
+            <div className="project__team">{ data.team }</div>
+          </div>
+          <div className="project__description">{ data.decription }</div>
+          <div className="project__picture">{ data.picture }</div>
+        </div>
+
   )
 }
