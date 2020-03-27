c50ad72583c3f45605fe729b243c96eca915b895
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-03-27 17:59:15 -0400

    minimized the project overview

--- src/components/project/project.overview.js
+++ src/components/project/project.overview.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { ProjectList } from './project.item';
 
-export default class ProjectOverviewPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <div className="page page__project">
-        <div className="page__project__items">
-          <ProjectList />
-        </div>
+export default function ProjectOverviewPage() {
+  return (
+    <div className="page page__project">
+      <div className="page__project__items">
+        <ProjectList />
       </div>
-    )
-  }
+    </div>
+  )
 }
