fb91991653ee6e24263b0f101cf770f8bcfaf4c3
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-03-30 16:48:01 -0400

    processes data shape correctly now

--- src/components/project/project.detail.js
+++ src/components/project/project.detail.js
@@ -3,12 +3,13 @@ import data from '../../data/data.project.json';
 import './project.detail.desktop.scss';
 import './project.detail.mobile.scss';
 
+import { InfoTable } from '../subcomponents/infotable/infotable.object';
+
 export class ProjectDetailPage extends React.Component {
   render() {
     const { projectId } = this.props.match.params;
     const info = data.projects[projectId];
     const errorQuote = "I'm afraid I can't do that Michael...";
-    console.log(data);
 
     return info === null || info === undefined
       ? <div className="error"><p>{errorQuote}</p></div>
@@ -19,16 +20,41 @@ export class ProjectDetailPage extends React.Component {
 
 export function ProjectDetail(data) {
   return (
-    
     <div className="project__detail">
       <div className="project__detail__image__main"></div>
       <div className="project__detail__name">
         {/* Name: { data.title } */}
         Fordham University
       </div>
-      <div className="project__detail__infotable"></div>
+      <div className="project__detail__infotable">
+        <ProjectInfoTable {...data} />
+      </div>
       <div className="project__detail__image__extras"></div>
       <div className="project__detail__summary"></div>
     </div>
   )
 }
+
+function ProjectInfoTable(data) {
+
+  const info = convertObjectDataToArray(data);
+  console.log(info);
+  return (
+    <InfoTable data={info} />
+  )
+}
+
+function convertObjectDataToArray(data) {
+  const names = Object.getOwnPropertyNames(data);
+
+  var array = names.map(n => {
+    var value = data[n];
+    return {
+      title: n,
+      value
+    }
+  });
+
+  return array;
+}
+
