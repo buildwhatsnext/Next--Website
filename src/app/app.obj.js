c503bf662fa86e36a0a0b9dc3b876814697f067f
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-03-30 18:15:10 -0400

    resolved mouse tracking & follower

--- src/app/app.obj.js
+++ src/app/app.obj.js
@@ -4,11 +4,41 @@ import AppCursor from './app.cursor';
 import './app.style.desktop.scss';
 import './app.style.mobile.scss';
 
-export default function App() {
-  return (
-    <div className="app">
-      <AppCursor />
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positionX: 0,
+      positionY: 0
+    }
+    this.updateMousePosition = this.updateMousePosition.bind(this);
+  }
+
+  updateMousePosition = (event) => {
+    event.preventDefault();
+    const position = {
+      x : event.clientX,
+      y : event.clientY
+    }
+
+    console.log('This is the positionX: ' + position.x);
+    console.log('This is the positionY: ' + position.y);
+
+    this.setState({
+      positionX: position.x,
+      positionY: position.y
+    });
+  }
+
+  render() {
+    return (
+    <div className="app" onMouseMove={this.updateMousePosition}>
+      <AppCursor 
+        positionX={ this.state.positionX } 
+        positionY={ this.state.positionY } 
+      />
       <AppRouter />
     </div>
-  )
+    )
+  }
 }
