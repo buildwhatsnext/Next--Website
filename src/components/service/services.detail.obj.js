91223e76399382a23a613d4f93c2135c04b4948b
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-03-31 15:31:09 -0400

    service detail is 80% done

--- src/components/service/services.detail.obj.js
+++ src/components/service/services.detail.obj.js
@@ -22,18 +22,22 @@ export function ServiceDetail(data) {
   return (
     <div className= "service__detail">
       <div className="service__detail__mainbox">
+        <div className="service__detail__box__coral"></div>
         <div className="service__detail__title">{ data.title }</div>
-        <div className="service__detail__desription">{ data.description }</div>
+        <div className="service__detail__description">{ data.description }</div>
+        <div className="cta_contactus"><p>Contact Us</p></div>
       </div>
       <div className="service__detail__outerbox">
         <div className="service__detail__subtitle1">{ data.subtitle1 }</div>
         <div className="service__detail__subtitle1description">{ data.subtitle1description }</div>
-        <div className="service__detail__subtitle1">{ data.subtitle2 }</div>
-        <div className="service__detail__subtitle1description">{ data.subtitle2description }</div>
-        <div className="service__detail__subtitle1">{ data.subtitle3 }</div>
-        <div className="service__detail__subtitle1description">{ data.subtitle3description }</div>
+        <div className="cta1"><p>See How</p></div>
+        <div className="service__detail__subtitle2">{ data.subtitle2 }</div>
+        <div className="service__detail__subtitle2description">{ data.subtitle2description }</div>
+        <div className="cta2"><p>See How</p></div>
+        <div className="service__detail__subtitle3">{ data.subtitle3 }</div>
+        <div className="service__detail__subtitle3description">{ data.subtitle3description }</div>
+        <div className="cta3"><p>See How</p></div>
       </div>
-
     </div>
   )
 }
