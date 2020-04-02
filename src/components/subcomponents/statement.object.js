120b5311a38555ec55ed0328d5da8bf6180e2fe0
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-02 10:06:36 -0400

    statement gone by using statement mutable

--- src/components/subcomponents/statement.object.js
+++ src/components/subcomponents/statement.object.js
@@ -6,8 +6,8 @@ import '../subcomponents/statements/statement.style.mobile.scss';
 
 export function StatementMutable(props) {
   const { data, statement } = props;
-  const [ index, setIndex ] = useState(0);
-  const [ display, setDisplay ] = useState(statement);
+  const [ index, setIndex ] = useState(1);
+  const [ display, setDisplay ] = useState('design');
 
   const incrementData = () => {
     setDisplay(data[index]);
@@ -20,7 +20,7 @@ export function StatementMutable(props) {
   }
 
   useEffect(() => {
-    setTimeout(incrementData, 1000);
+    setTimeout(incrementData, 2000);
   });
 
   return (
