443ea3e1e8057dfb48e6754975b02becd73bb5e4
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-04-03 10:23:32 -0400

    corrected colors on nav icons

--- src/components/subcomponents/navigation/nav.obj.js
+++ src/components/subcomponents/navigation/nav.obj.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import './nav.desktop.scss';
 import './nav.mobile.scss';
 import logo from '../../../assets/logo_next.svg';
