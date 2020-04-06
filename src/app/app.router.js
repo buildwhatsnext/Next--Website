3309f52eeaa7c081264eb6fc50d41e01d9d198ec
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-04-06 11:27:12 -0400

    still reworking the router but fixed the css situation

--- src/app/app.router.js
+++ src/app/app.router.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { TimelineMax as Timeline, Power1 } from 'gsap';
 import { Transition, TransitionGroup } from 'react-transition-group';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navbar from '../components/subcomponents/navigation/nav.obj';
@@ -35,9 +36,11 @@ const Routes = (props) => (
 )
 
 const AnimatedRoutes = (props) => (
-  <TransitionGroup>
+  <TransitionGroup component={null}>
     <Transition
-      // appear={true}
+      appear={true}
+      onEnter={(node, appears) => play(props.pathname, node, appears)}
+      timeout={{enter: 750, exit: 0}}
     >
       <RouteCollection {...props} />
     </Transition>
@@ -46,16 +49,47 @@ const AnimatedRoutes = (props) => (
 
 const RouteCollection = (props) => (
   <Switch>
-    <Route path='/about'>
-      <About />
-    </Route>
-    <Route path='/contact'>
-      <Contact />
-    </Route>
+    <Route path='/about' render={(props) => (<About {...props}/> )} />
+    <Route path='/contact' render={(props) => (<Contact {...props}/> )} />
     <Route path="/team" render={(props) => (<TeamRouter {...props} />)} />
     <Route path="/services" render={(props) => (<ServiceRouter {...props} />)} />
     <Route path="/projects" render={(props) => (<ProjectRouter {...props} />)} />
-    <Route exact path='/' render={(props) => (<HomePage {...props}/>
-    )}/>
+    <Route exact path='/' render={(props) => (<HomePage {...props}/>)} />
   </Switch>
 )
+
+export const play = (pathname, node, appears) => {
+  const delay = appears ? 0 : 0.5
+  let timeline
+
+  if (pathname === '/')
+    timeline = getHomeTimeline(node, delay)
+  else
+    timeline = getDefaultTimeline(node, delay)
+
+  timeline.play()
+}
+
+const getHomeTimeline = (node, delay) => {
+  const timeline = new Timeline({ paused: true });
+  const texts = node.querySelectorAll('h1 > div');
+
+  timeline
+    .from(node, 0, { display: 'none', autoAlpha: 0, delay })
+    .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+
+  return timeline
+}
+
+const getDefaultTimeline = (node, delay) => {
+  const timeline = new Timeline({ paused: true });
+  const content = node.querySelector('.content');
+  const contentInner = node.querySelector('.content--inner');
+
+  timeline
+    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
+    .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
+    .from(contentInner, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
+
+  return timeline;
+}
