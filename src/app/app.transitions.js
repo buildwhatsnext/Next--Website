dc13faed516b68c155bec0b571fa411426b81b21
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-13 15:56:22 -0400

    my own implementation now

--- src/app/app.transitions.js
+++ src/app/app.transitions.js
@@ -27,9 +27,9 @@ const getHomeTimeline = (node, delay) => {
   const timeline = new Timeline({ paused: true });
   const texts = node.querySelectorAll('h1 > div');
 
-  timeline
-    .from(node, 0, { display: 'none', autoAlpha: 0, delay })
-    .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+  // timeline
+    // .from(node, 0, { display: 'none', autoAlpha: 0, delay })
+    // .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
 
   return timeline
 }
@@ -63,9 +63,17 @@ const getDefaultTimeline = (node, delay) => {
 const getContactTimeline = (node, delay) => {
   const timeline = new Timeline({ paused: true });
   const title = node.querySelector('.contact__title > p');
+  const quote1 = node.querySelector('.contact__quote__p01 > p');
+  const quote2 = node.querySelector('.contact__quote__p02 > p');
+  const quote3 = node.querySelector('.contact__quote__p03 > p');
+  const quote4 = node.querySelector('.contact__quote__p04 > p');
   timeline
-    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-    .from(title, 0.5, {y: 100})
+    .from(node, 0.5, { y: 10, display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
+    .from(title, 0.5, {y: 100}, 1)
+    .from(quote1, 0.5, {delay: 0.1, y: 100}, 1)
+    .from(quote2, 0.5, {delay: 0.2, y: 100}, 1)
+    .from(quote3, 0.5, {delay: 0.3, y: 100}, 1)
+    .from(quote4, 0.5, {delay: 0.4, y: 100}, 1)
 
   return timeline;
 }
