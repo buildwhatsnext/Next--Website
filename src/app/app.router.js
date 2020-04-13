88dd3aaefdd4530aa9d8695a98d699263b9eb4d5
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-04-13 11:57:45 -0400

    doing simple transitions

--- src/app/app.router.js
+++ src/app/app.router.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
-import { TimelineMax as Timeline, Power1 } from 'gsap';
 import { Transition, TransitionGroup } from 'react-transition-group';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Navbar from '../components/subcomponents/navigation/nav.obj';
 import HomePage from '../components/home/home.page';
 import Contact from '../components/contact/contact.obj';
@@ -9,6 +9,8 @@ import About from '../components/about/about.obj';
 import { ServiceRouter } from '../components/service/services.obj';
 import { TeamRouter } from '../components/team/team.obj';
 import { ProjectRouter } from '../components/project/project.obj';
+
+import { play } from './app.transitions';
 
 import '../styles/styles.interaction.scss';
 
@@ -62,39 +64,3 @@ const RouteCollection = (props) => (
     <Route exact path='/' render={(props) => (<HomePage {...props}/>)} />
   </Switch>
 )
-
-export const play = (pathname, node, appears) => {
-  const delay = appears ? 0 : 0.5
-  let timeline
-
-  if (pathname === '/')
-    timeline = getHomeTimeline(node, delay)
-  else
-    timeline = getDefaultTimeline(node, delay)
-
-  timeline.play()
-}
-
-const getHomeTimeline = (node, delay) => {
-  const timeline = new Timeline({ paused: true });
-  const texts = node.querySelectorAll('h1 > div');
-
-  timeline
-    .from(node, 0, { display: 'none', autoAlpha: 0, delay })
-    .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
-
-  return timeline
-}
-
-const getDefaultTimeline = (node, delay) => {
-  const timeline = new Timeline({ paused: true });
-  const content = node.querySelector('.team__item');
-  const contentInner = node.querySelector('.team__item__block');
-
-  timeline
-    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-    .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
-    .from(contentInner, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
-
-  return timeline;
-}
---
+++ src/app/app.transitions.js
@@ -0,0 +1,58 @@
+import { TimelineMax as Timeline, Power1 } from 'gsap';
+
+export const play = (pathname, node, appears) => {
+  const delay = appears ? 0 : 0.5
+  let timeline;
+  console.log(pathname);
+
+  switch(pathname) {
+    case '/':
+      timeline = getHomeTimeline(node, delay)
+      break;
+    case '/team':
+      timeline = getTeamTimeline(node, delay);
+      break;
+    default:
+      timeline = getDefaultTimeline(node, delay);
+      break;
+  }
+
+  timeline.play()
+}
+
+const getHomeTimeline = (node, delay) => {
+  const timeline = new Timeline({ paused: true });
+  const texts = node.querySelectorAll('h1 > div');
+
+  timeline
+    .from(node, 0, { display: 'none', autoAlpha: 0, delay })
+    .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+
+  return timeline
+}
+
+const getTeamTimeline = (node, delay) => {
+  const timeline = new Timeline({ paused: true });
+  const members = node.querySelectorAll('.team__item__image');
+  const blocks = node.querySelectorAll('.team__item__block__content');
+
+  timeline
+    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
+    .from(blocks, .5, { x: -500, ease: Power1.easeOut })
+    .from(members, 1.5, { autoAlpha: 0 , ease: Power1.easeInOut });
+
+  return timeline;
+}
+
+const getDefaultTimeline = (node, delay) => {
+  const timeline = new Timeline({ paused: true });
+  const members = node.querySelectorAll('.team__item__image');
+  const blocks = node.querySelectorAll('.team__item__block__content');
+
+  // timeline
+  //   .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
+  //   .from(blocks, .5, { y: 500, ease: Power1.easeOut })
+  //   .from(members, 1.5, { autoAlpha: 0 , ease: Power1.easeInOut });
+
+  return timeline;
+}
