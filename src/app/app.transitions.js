bda141484b321165f404f3b90b977ef116e4608b
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-04-14 15:53:39 -0400

    animated team  member name, position & location

--- src/app/app.transitions.js
+++ src/app/app.transitions.js
@@ -47,7 +47,7 @@ const getProjectTimeline = (node, delay) => {
   console.log(node);
 
   timeline
-    // .from(node, 0, { display: 'none', autoAlpha: 0, delay })
+    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay })
     .from(projects, 0.5, { y: 500 , stagger: 0.25, ease: Power1.easeOut } );
 
   return timeline;
@@ -57,24 +57,26 @@ const getTeamTimeline = (node, delay) => {
   const timeline = new Timeline({ paused: true });
   const members = node.querySelectorAll('.team__item__image');
   const blocks = node.querySelectorAll('.team__item__block__content');
+  const name = node.querySelectorAll('.team__item__name');
+  const position = node.querySelectorAll('.team__item__position');
+  const location = node.querySelectorAll('.team__item__location');
 
   timeline
-    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-    .from(blocks, .5, { x: -500, stagger: .1, ease: Power1.easeOut, }, 0)
-    .from(members, .5, { delay: 0.1, autoAlpha: 0, stagger: .1 , x: -10 , ease: Power1.easeInOut }, 0);
+    .from(node, 0.5, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn }, 0)
+    .from(blocks, .5, { x: -500, stagger: .1, ease: Power1.easeOut, }, 1)
+    .from(members, .5, { delay: 0.1, autoAlpha: 0, stagger: .1 , x: -10 , ease: Power1.easeInOut }, 1)
+    .from(name, .5, { delay: 0.1, autoAlpha: 0, stagger: .1 , x: -10 , ease: Power1.easeInOut }, 2)
+    .from(position, .5, { delay: 0.1, autoAlpha: 0, stagger: .1 , x: -10 , ease: Power1.easeInOut }, 2)
+    .from(location, .5, { delay: 0.1, autoAlpha: 0, stagger: .1 , x: -10 , ease: Power1.easeInOut }, 2)
 
   return timeline;
 }
 
 const getDefaultTimeline = (node, delay) => {
   const timeline = new Timeline({ paused: true });
-  const members = node.querySelectorAll('.team__item__image');
-  const blocks = node.querySelectorAll('.team__item__block__content');
 
-  // timeline
-  //   .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-  //   .from(blocks, .5, { y: 500, ease: Power1.easeOut })
-  //   .from(members, 1.5, { autoAlpha: 0 , ease: Power1.easeInOut });
+  timeline
+    .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
 
   return timeline;
 }
