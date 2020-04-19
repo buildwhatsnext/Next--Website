b5bd09f41f127cb390f540aac2f7b3ac7a5c10d9
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-19 14:52:02 -0400

    react-slick trying to be applied

--- src/components/about/about.detail.obj.js
+++ src/components/about/about.detail.obj.js
@@ -5,10 +5,26 @@ import { AboutTitle } from '../subcomponents/about_titlepage/titlepage.obj';
 import { AboutCategory } from '../subcomponents/about_category/category.obj';
 import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
 import data from '../../data/data.about.json';
+import Slider from "react-slick;"
 
 
 export default class AboutBranchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.next=this.next.bind(this);
+    this.seehow=this.seehow.bind(this);
+  }
+
+  next() {
+    this.slider.slickNext();
+  }
+
+  seehow() {
+    this.slider.slickSeeHow();
+  }
+
   render() {
+
     const { aboutBranch } = this.props.match.params;
     const info = data.chapter[aboutBranch];
     const errorQuote = "I'm afraid I can't do that Michael...";
@@ -19,27 +35,51 @@ export default class AboutBranchPage extends React.Component {
   }
 }
 
-  export function AboutPage(data) {
-  
-    const next = "Next";
-    const seehow = "See How";
-  
-    return (
-      <div className="about">
 
-        <div className={`about__background about__background__${data.shortName}`}/>
-        <div className={`about__icon about__icon__${data.shortName}`}/>
+export function AboutPage(data) {
+  
+  const next = "Next";
+  const seehow = "See How";
 
-        <div className="about__textgrid">
-          <div className="about__title"><p>{data.name}</p></div>
-          <div className="about__description"><p>{data.description}</p></div>
-          <div className="about__seehow">
-            <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
-          </div>
-          <div className="about__back">
-            <InternalLinkHighlighted value={next} destination="/about/second" />
-          </div>
-        </div>
-      </div>
-    )
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    slidesToScroll: 1
   }
+
+  return (
+    <div className="about">
+      <Slider ref={c => (this.slider = c)} {...settings}>
+        <div key={1}>
+          <h3>1</h3>
+        </div>
+      </Slider>
+    </div>
+  )
+}
+
+  // export function AboutPage(data) {
+  
+  //   const next = "Next";
+  //   const seehow = "See How";
+  
+  //   return (
+  //     <div className="about">
+
+  //       <div className={`about__background about__background__${data.shortName}`}/>
+  //       <div className={`about__icon about__icon__${data.shortName}`}/>
+
+  //       <div className="about__textgrid">
+  //         <div className="about__title"><p>{data.name}</p></div>
+  //         <div className="about__description"><p>{data.description}</p></div>
+  //         <div className="about__seehow">
+  //           <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
+  //         </div>
+  //         <div className="about__back">
+  //           <InternalLinkHighlighted value={next} destination="/about/second" />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
