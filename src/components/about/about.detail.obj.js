bc9e25e892e79f07d7ed2ba64d222798c76dbaa8
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-20 10:56:43 -0400

    state in

--- src/components/about/about.detail.obj.js
+++ src/components/about/about.detail.obj.js
@@ -6,54 +6,27 @@ import { AboutTitle } from '../subcomponents/about_titlepage/titlepage.obj';
 import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
 import data from '../../data/data.about.json';
 
-const arrayData= [
-  {
-    id:'1',
-    title: 'Craft Inclusive Experiences',
-    shortname: 'experiences',
-    description: 'We craft immersive, narrative-driven, and collaborative experiences for project stakeholders.'
-  },
-
-  {
-    id:'2',
-    title: 'Elevate Capabilities',
-    shortname: 'experiences',
-    description: 'We help build structure, resources, knowledge to help project teams run smarter.'
-  },
-
-  {
-    id:'3',
-    title: 'Create Insight',
-    shortname: 'insight',
-    description: 'We create a platform for intelligent decision-making in the design process.'
-  },
-
-  {
-    id:'4',
-    title: 'Invent',
-    shortname: 'invent',
-    description: 'If a solution to design problem exists we find it, if it doesn’t, we invent it'
-  },
-
-  {
-    id:'5',
-    title: 'Engage',
-    shortname: 'engage',
-    description: 'Innovation often begins with two people having a conversation. We are always open to talk.'
-  },
-];
-
-for (let i = 0; i < arrayData.length; i++) {
-  console.log(arrayData[i]);
-}
-
 export default class AboutBranchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      aboutPage: this.props.data
+    };
+    this.nextPage = this.nextPage.bind(this);
+  }
+
+  nextPage() {
+    const aboutCarousel = this.state.aboutPage + 1;
+    this.setState({
+      aboutPage: aboutCarousel
+    });
+  }
   
 
   render() {
 
     const { aboutBranch } = this.props.match.params;
-    const info = data.chapter[aboutBranch];
+    const info = data.aboutData[aboutBranch];
     const errorQuote = "I'm afraid I can't do that Michael...";
 
     return info === null || info === undefined
