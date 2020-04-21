535135e15a6e60a910839e47975e2e8ecea866f9
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-21 19:03:59 -0400

    trying to turn about back to normal..

--- src/components/about/about.obj.js
+++ src/components/about/about.obj.js
@@ -1,36 +1,45 @@
-import React, { Fragment } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Fragment, useRef, useEffect } from 'react';
+import { TweenMax, Timeline, Power1 } from 'gsap';
 import './about.style.desktop.scss';
 import './about.style.mobile.scss';
-import data from '../../data/data.about.json';
-import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
-import AboutRoutePage from './about.detail.obj';
+import { AboutTitle } from '../subcomponents/about_titlepage/titlepage.obj';
+import { AboutCategory } from '../subcomponents/about_category/category.obj';
+
 
 export default class About extends React.Component {
+  constructor(props) {
+    super (props);
+    this.myPink=null;
+    
+  }
 
-  
+  componentDidMount(){
+    this.myPink = TweenMax.from(this.myPink, 1.5, {y: 700, ease: Power1.easeInOut})
+    this.myWhite = TweenMax.from(this.myWhite, 1.5, {delay: 1.75, opacity: 0, x: 20, y: 20, ease: Power1.easeOut})
+  }
+
+
+  setName = (name) => {
+      this.setState({name});
+  }
+
   render() {
-
-    const next = "Next";
-    const seehow = "See How";
-
     return (
-      <div className="about">
-
-        <div className={`about__background about__background__${data.shortName}`}/>
-        <div className={`about__icon about__icon__${data.shortName}`}/>
-
-        <div className="about__textgrid">
-          <div className="about__title"><p>{data.name}</p></div>
-          <div className="about__description"><p>{data.description}</p></div>
-          <div className="about__seehow">
-            <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
+      <Fragment>
+        <div className="about">       
+          <div className="about__titlepage">  
+            <div className="about__coralcard">
+              <AboutTitle />
+            </div>
+            <div className="about__whitecard"></div>
           </div>
-          <div className="about__next">
-            <InternalLinkHighlighted value={next} destination="/about/second" />
-          </div>
+
+          <div className="about__category">
+            <AboutCategory />
+          </div>  
         </div>
-      </div>
+        
+      </Fragment>
     )
   }
 }
