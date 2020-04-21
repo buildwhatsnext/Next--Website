7bf605e1a962955a1ed7056d7b3135553b2de2e0
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-21 16:49:23 -0400

    About came back to normal?

--- src/components/about/about.obj.js
+++ src/components/about/about.obj.js
@@ -2,67 +2,35 @@ import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './about.style.desktop.scss';
 import './about.style.mobile.scss';
-import aboutData from '../../data/data.about.json';
+import data from '../../data/data.about.json';
 import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
 import AboutRoutePage from './about.detail.obj';
 
-const dataSet = ["red", "yellow", "green", "blue", "purple"];
-
 export default class About extends React.Component {
-  constructor(props) {
-    super (props);
-    this.state = {
-      pageNumber: 0 + {dataSet[0]}
-    };
 
-    this.aboutPage = this.aboutPage.bind(this);
-  }
-
-  aboutPage() {
-    const currentPage = this.state.pageNumber + 1;
-    this.setState({
-      pageNumber: currentPage
-    })
-  }
-
-    
+  
   render() {
 
+    const next = "Next";
+    const seehow = "See How";
+
     return (
-      <div>
-        <p>{this.state.pageNumber}</p>
-        {/* <Button action={this.currentPage} label="Next" /> */}
+      <div className="about">
+
+        <div className={`about__background about__background__${data.shortName}`}/>
+        <div className={`about__icon about__icon__${data.shortName}`}/>
+
+        <div className="about__textgrid">
+          <div className="about__title"><p>{data.name}</p></div>
+          <div className="about__description"><p>{data.description}</p></div>
+          <div className="about__seehow">
+            <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
+          </div>
+          <div className="about__next">
+            <InternalLinkHighlighted value={next} destination="/about/second" />
+          </div>
+        </div>
       </div>
     )
   }
-
-  // const Button = ({action, label}) => (
-  //   <button onClick={() => action()}>{label}</button>
-  // )
-
-  
-  // render() {
-
-  //   const next = "Next";
-  //   const seehow = "See How";
-
-  //   return (
-  //     <div className="about">
-
-  //       <div className={`about__background about__background__${data.shortName}`}/>
-  //       <div className={`about__icon about__icon__${data.shortName}`}/>
-
-  //       <div className="about__textgrid">
-  //         <div className="about__title"><p>{data.name}</p></div>
-  //         <div className="about__description"><p>{data.description}</p></div>
-  //         <div className="about__seehow">
-  //           <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
-  //         </div>
-  //         <div className="about__back">
-  //           <InternalLinkHighlighted value={next} destination="/about/second" />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   )
-  // }
 }
