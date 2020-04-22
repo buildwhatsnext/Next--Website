54ac94c73fe6df6005dc04a119f92d995ccea93a
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-04-22 09:10:05 -0400

    back to normal?

--- src/components/about/about.detail.obj.js
+++ src/components/about/about.detail.obj.js
@@ -1,46 +1,46 @@
 
-// import React, { Fragment } from 'react';
-// import './about.style.desktop.scss';
-// import './about.style.mobile.scss';
-// import { AboutTitle } from '../subcomponents/about_titlepage/titlepage.obj';
-// import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
-// import data from '../../data/data.about.json';
+import React, { Fragment } from 'react';
+import './about.style.desktop.scss';
+import './about.style.mobile.scss';
+import { AboutTitle } from '../subcomponents/about_titlepage/titlepage.obj';
+import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
+import data from '../../data/data.about.json';
 
-// export default class AboutBranchPage extends React.Component {
+export default class AboutBranchPage extends React.Component {
 
-//   render() {
+  render() {
 
-//     const { aboutBranch } = this.props.match.params;
-//     const info = data.aboutData[aboutBranch];
-//     const errorQuote = "I'm afraid I can't do that Michael...";
+    const { aboutBranch } = this.props.match.params;
+    const info = data.aboutData[aboutBranch];
+    const errorQuote = "I'm afraid I can't do that Michael...";
 
-//     return info === null || info === undefined
-//       ? <div className="error"><p>{errorQuote}</p></div>
-//       : <AboutPage {...info} />
-//   }
-// }
+    return info === null || info === undefined
+      ? <div className="error"><p>{errorQuote}</p></div>
+      : <AboutPage {...info} />
+  }
+}
 
-//   export function AboutPage(data) {
+  export function AboutPage(data) {
   
-//     const next = "Next";
-//     const seehow = "See How";
+    const next = "Next";
+    const seehow = "See How";
   
-//     return (
-//       <div className="about">
+    return (
+      <div className="about">
 
-//         <div className={`about__background about__background__${data.shortName}`}/>
-//         <div className={`about__icon about__icon__${data.shortName}`}/>
+        <div className={`about__background about__background__${data.shortName}`}/>
+        <div className={`about__icon about__icon__${data.shortName}`}/>
 
-//         <div className="about__textgrid">
-//           <div className="about__title"><p>{data.name}</p></div>
-//           <div className="about__description"><p>{data.description}</p></div>
-//           <div className="about__seehow">
-//             <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
-//           </div>
-//           <div className="about__back">
-//             <InternalLinkHighlighted value={next} destination="/about/second" />
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
+        <div className="about__textgrid">
+          <div className="about__title"><p>{data.name}</p></div>
+          <div className="about__description"><p>{data.description}</p></div>
+          <div className="about__seehow">
+            <InternalLinkHighlighted value={seehow} destination="/projects/piper" />
+          </div>
+          <div className="about__next">
+            <InternalLinkHighlighted value={next} destination="/about/second" />
+          </div>
+        </div>
+      </div>
+    )
+  }
