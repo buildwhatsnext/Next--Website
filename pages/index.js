7ff894fe1792d990e45060d195d4d8734988595d
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-05-09 13:30:55 -0400

    added gsap & a prestart cmd

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Head from 'next/head'
+import { gsap } from 'gsap';
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollToPlugin);
 
 export default function Home() {
   const line1 = 'Next is a startup';
   const line2 = "Here's some stuff to talk about";
   const line3 = "These are some things we're working on";
+
+  const scrollScreen = (id) => {
+    gsap.to(window, { 
+      duration: 2, 
+      scrollTo: {
+        y: `#section${id}`,
+        offset: 50
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -18,6 +32,7 @@ export default function Home() {
             <h1>{ line1 }</h1>
             <h1>{ line2 }</h1>
             <h1>{ line3 }</h1>
+            <p onClick={(e) => scrollScreen("2") }>This should scroll to section 2</p>
           </div>
         </section>
         <section className="section">
