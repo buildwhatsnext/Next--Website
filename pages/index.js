3d60e91725f1018b4c77417a661445fc7bf7eddf
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-05-11 09:57:17 -0400

    added a shaped background

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import Head from 'next/head'
 import { gsap } from 'gsap';
-import ScrollToPlugin from 'gsap/ScrollToPlugin';
+// import ScrollToPlugin from 'gsap/ScrollToPlugin';
 
-gsap.registerPlugin(ScrollToPlugin);
+// gsap.registerPlugin(ScrollToPlugin);
 
 export default function Home() {
   const line1 = 'Next is a startup';
   const line2 = "Here's some stuff to talk about";
   const line3 = "These are some things we're working on";
 
-  const scrollScreen = (id) => {
-    gsap.to(window, { 
-      duration: 2, 
-      scrollTo: {
-        y: `#section${id}`,
-        offset: 50
-      }
-    });
-  }
+  // const scrollScreen = (id) => {
+  //   gsap.to(window, { 
+  //     duration: 2, 
+  //     scrollTo: {
+  //       y: `#section${id}`,
+  //       offset: 50
+  //     }
+  //   });
+  // }
 
   return (
     <div className="container">
@@ -27,16 +27,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <section className="section">
-          <div className="title" id="1">
+        <section className="section section_first grid">
+          <div className="background trap"/>
+          <div className="first" id="1">
             <h1>{ line1 }</h1>
             <h1>{ line2 }</h1>
             <h1>{ line3 }</h1>
-            <p onClick={(e) => scrollScreen("2") }>This should scroll to section 2</p>
+            {/* <p onClick={(e) => scrollScreen("2") }>This should scroll to section 2</p> */}
           </div>
         </section>
-        <section className="section">
-          <div className="title" id="2">
+        <section className="section section_second">
+          <div className="second" id="2">
             <h1>Second stuff</h1>
             <h1>Second stuff  dfasfsdf </h1>
             <h1>More to come!</h1>
