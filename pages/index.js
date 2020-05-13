fbefbd5816a354d3756a79c34e7bec2e4d3b3f9a
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-05-13 14:04:57 -0400

    Revert "exploring the tweened elements example"
    This reverts commit edb514803f8ee53613d521feb668866a03b25f60.

--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,66 @@
 import Head from 'next/head'
-import { Controller, Scene } from 'react-scrollmagic';
-import { Tween, Timeline } from 'react-gsap';
+import Navbar from '../objects/navbar.object';
+import { gsap } from 'gsap';
 
 export default function Home() {
+  const line1 = 'Next is a startup';
+  const line2 = "Here's some stuff to talk about";
+  const line3 = "These are some things we're working on";
+
+  // const scrollScreen = (id) => {
+  //   gsap.to(window, { 
+  //     duration: 2, 
+  //     scrollTo: {
+  //       y: `#section${id}`,
+  //       offset: 50
+  //     }
+  //   });
+  // }
 
   return (
-    <div>
-      <div className="section" />
-      <div id="trigger" />
-      <Controller>
-        <Scene
-          triggerElement="#trigger"
-          duration={300}
-        >
-          {(progress) => (
-            <Tween            
-              to={{
-                left: '0px',
-                rotation: -360,
-                width: '200px',
-                height: '200px',
-              }}       
-              ease="Strong.easeOut"
-              totalProgress={progress}
-              paused
-            >
-              <div className="tween">Pin Test</div>
-            </Tween>    
-          )}
-        </Scene>
-        <Scene
-          triggerElement="#trigger"
-          duration={600}
-          pin={false}
-        >
-          {(progress) => (
-            <Tween
-              from={{
-                css: {
-                  left: '0px',
-                  rotation: -360,
-                  width: '200px',
-                  height: '200px',
-                },
-                ease: 'Strong.easeOut',
-              }}
-              to={{
-                css: {
-                  left: '100px',
-                  rotation: -180,
-                  width: '50px',
-                  height: '50px',
-                },
-                ease: 'Strong.easeOut',
-              }}
-              totalProgress={progress}
-              paused
-            >
-              <div className="tween">Pin Test</div>
-            </Tween>
-          )}        
-        </Scene>
-        <Scene
-          duration={500}
-        >
-          {(progress) => (
-            <div>
-              <Tween        
-                from={{
-                  left: 700,
-                }}
-                to={{
-                  left: 0,
-                  ease: 'Back.easeOut',
-                }}
-                stagger={0.15}
-                totalProgress={progress}
-                paused
-              >
-                <div className="stagger" />
-                <div className="stagger" />
-                <div className="stagger" />
-                <div className="stagger" />
-                <div className="stagger" />
-              </Tween>
-            </div>
-          )}
-        </Scene>
-      </Controller>
-      <div className="section" />
+    <div className="container">
+      <Head>
+        <title>Next/ Design Tech Startup? </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <main>
+        {/* <Navbar />   */}
+        <div className="nav"><h1>NEXT</h1></div>
+        <section className="section section_first grid">
+          <div className="background trap"/>
+          <div className="first" id="1">
+            <h1>{ line1 }</h1>
+            <h1>{ line2 }</h1>
+            <h1>{ line3 }</h1>
+            {/* <p onClick={(e) => scrollScreen("2") }>This should scroll to section 2</p> */}
+          </div>
+        </section>
+        <section className="section section_second">
+          <div className="second" id="2">
+            <h1>Second stuff</h1>
+            <h1>Second stuff  dfasfsdf </h1>
+            <h1>More to come!</h1>
+          </div>
+        </section>
+
+        <section className="section section_third">
+          <div className="third" id="3">
+            <h1>Third stuff</h1>
+            <h1>Third stuff dsal;jjshflkshf </h1>
+            <h1>Not much left</h1>
+          </div>
+        </section>
+
+        <section className="section section_fourth">
+          <div className="fourth" id="4">
+            <h1>Fourth stuff</h1>
+            <h1>Fourth portion!</h1>
+            <h1>This is the end</h1>
+          </div>
+        </section>
+      </main>
+
     </div>
-    
   )
 }
