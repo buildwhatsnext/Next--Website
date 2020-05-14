136e23406e340e24d89fbe5e4e068786bc33ce90
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-05-14 15:02:24 -0400

    testing out original gsap stuff

--- objects/tweens.object.js
+++ objects/tweens.object.js
@@ -1,57 +1,5 @@
 import { Tween, Timeline } from 'react-gsap';
 
-export function First(props) {
-  const { tweenProgress } = props;
-  // console.log(tweenProgress);
-
-  return (
-    <Tween            
-      to={{
-        left: '0px',
-        rotation: -360,
-        width: '200px',
-        height: '200px',
-      }}       
-      ease="Strong.easeOut"
-      totalProgress={ tweenProgress }
-      paused
-    >
-      <div className="tween">Pin Test Example</div>
-    </Tween>    
-  )
-}
-
-export function Second(props) {
-  const { prog } = props;
-
-  return (
-    <Tween
-      from={{
-        css: {
-          left: '0px',
-          rotation: -360,
-          width: '200px',
-          height: '200px',
-        },
-        ease: 'Strong.easeOut',
-      }}
-      to={{
-        css: {
-          left: '100px',
-          rotation: -180,
-          width: '50px',
-          height: '50px',
-        },
-        ease: 'Strong.easeOut',
-      }}
-      totalProgress={ prog }
-      paused
-    >
-      <div className="tween">Pin Test</div>
-    </Tween>
-  )
-}
-
 export function Third(props) {
   const { prog } = props;
 
--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,34 @@
 import Head from 'next/head'
 import { Controller, Scene } from 'react-scrollmagic';
-import { Tween, Timeline } from 'react-gsap';
-
-import { First, Second, Third } from '../objects/tweens.object';
+// import { Tween, Timeline } from 'react-gsap';
+import { TimelineMax as Timeline, Power1, gsap } from 'gsap';
+import { Third } from '../objects/tweens.object';
 
 export default function Home() {
-
   return (
-    <div>
-      <div className="section" />
-      <div id="trigger" />
-      <Controller
-      >
-
-        <Scene
-          triggerElement="#trigger"
-          duration={ 300 }
-          indicators={
-            true
-          }
-        >
-          {(progress) => (
-            <First tweenProgress={progress} />
-          )}
-        </Scene>
-        
-
-        <Scene
-          triggerElement="#trigger"
-          duration={600}
-          pin={false}
-        >
-          {(progress) => (
-            <Second prog={progress} />
-          )}        
-        </Scene>
-        <Scene
-          duration={500}
-        >
-          {(progress) => (
-
-            <Third prog={progress} />
-
-            // <div>
-            //   <Tween        
-            //     from={{
-            //       left: 700,
-            //     }}
-            //     to={{
-            //       left: 0,
-            //       ease: 'Back.easeOut',
-            //     }}
-            //     stagger={0.15}
-            //     totalProgress={progress}
-            //     paused
-            //   >
-            //     <div className="stagger" />
-            //     <div className="stagger" />
-            //     <div className="stagger" />
-            //     <div className="stagger" />
-            //     <div className="stagger" />
-            //   </Tween>
-            // </div>
-          )}
-        </Scene>
-      </Controller>
-      <div className="section" />
-    </div>
-    
+    <section 
+      onMouseDown={(e) => setInitialState(e)}
+    >
+      <div className="home">
+        <h1>This Is A Test</h1>
+      </div>
+    </section>
   )
+}
+
+function setInitialState(event) {
+  event.preventDefault();
+  console.log('Mouse clicked');
+
+  const node = event.currentTarget;
+  const timeline = new Timeline({ paused: true });
+  const home = node.querySelector('.home');
+  const text = node.querySelector('.home > h1');
+
+  console.log(home);
+
+  timeline
+    .to(home, .9, { backgroundColor: 'black' })
+
+  timeline.play();
 }
