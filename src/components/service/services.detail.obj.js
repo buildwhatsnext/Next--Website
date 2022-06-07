f8ba43c79471b23832ddddd8375f29ee8aa0a7d6
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2022-06-07 17:55:12 -0400

    got rid of description title from services page. also did minor restyling

--- src/components/service/services.detail.obj.js
+++ src/components/service/services.detail.obj.js
@@ -25,28 +25,30 @@ export function ServiceDetail(data) {
 
   return (
     <div className="service">
+      
+      <div className="service__right">
+        <div className={`service__right__icon service__right__icon__${data.slug}`}></div>
+        <div className='service__right__back'>
+          <InternalLinkHighlighted value={back} destination="/services" />
+        </div>
+      </div>
+      
       <div className="service__left">
         <div className="service__left__title">
           {data.title}
         </div>
-        <div className={`service__left__icon service__left__icon__${data.slug}`}></div>
-      </div>
-      <div className="service__right">
-        <div className="service__right__switch">
-          <div className={`service__right__switch__item service__right__switch__item__${data.slug}`}>
-            <div className='service__right__switch__item__name service__right__switch__item__name__1'>{data.servicename1}</div>
-            <div className='service__right__switch__item__text service__right__switch__item__text__1'>{data.servicename1text}</div>
-            <div className='service__right__switch__item__name service__right__switch__item__name__2'>{data.servicename2}</div>
-            <div className='service__right__switch__item__text service__right__switch__item__text__2'>{data.servicename2text}</div>
-            <div className='service__right__switch__item__name service__right__switch__item__name__3'>{data.servicename3}</div>
-            <div className='service__right__switch__item__text service__right__switch__item__text__3'>{data.servicename3text}</div>
+        <div className="service__left__switch">
+          <div className={`service__left__switch__item service__right__switch__item__${data.slug}`}>
+            <div className='service__left__switch__item__name service__right__switch__item__name__1'>{data.servicename1}</div>
+            <div className='service__left__switch__item__text service__right__switch__item__text__1'>{data.servicename1text}</div>
+            <div className='service__left__switch__item__name service__right__switch__item__name__2'>{data.servicename2}</div>
+            <div className='service__left__switch__item__text service__right__switch__item__text__2'>{data.servicename2text}</div>
+            <div className='service__left__switch__item__name service__right__switch__item__name__3'>{data.servicename3}</div>
+            <div className='service__left__switch__item__text service__right__switch__item__text__3'>{data.servicename3text}</div>
           </div>
         </div>
-        <div className={`service__right__descriptiontitle service__right__descriptiontitle__${data.slug}`}>{data.descriptiontitle}</div>
-        <div className={`service__right__descriptiontext service__right__descriptiontext__${data.slug}`}><p><mark>{data.description}</mark></p></div>
-        <div className='service__right__back'>
-          <InternalLinkHighlighted value={back} destination="/services" />
-        </div>
+        {/* <div className={`service__left__descriptiontitle service__right__descriptiontitle__${data.slug}`}>{data.descriptiontitle}</div> */}
+        <div className={`service__left__descriptiontext service__right__descriptiontext__${data.slug}`}><p><mark>{data.description}</mark></p></div>
       </div>
     </div>
   )
