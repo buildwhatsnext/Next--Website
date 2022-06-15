6df6cfacff79a9b2b0c3dd963073369840cf4831
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2022-06-14 22:57:19 -0400

    callback and login pages swapped

--- src/components/callback/callback.obj.js
+++ src/components/callback/callback.obj.js
@@ -1,94 +1,91 @@
 import React from 'react';
 import './callback.style.desktop.scss';
-import './callback.style.mobile.scss';
+// import './login.style.mobile.scss';
 import data from '../../data/data.callback.json';
-import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
-import {
-  BrowserRouter as Router,
-  Link,
-  useLocation
-} from "react-router-dom";
-
-// A custom hook that builds on useLocation to parse the query string.
-function useQuery() {
-  const { search } = useLocation();
-
-  return React.useMemo(() => new URLSearchParams(search), [search]);
-}
-
-function QueryParamsDemo() {
-  let query = useQuery();
-  const code = query.get("code")
-}
+import { ExternalLinkHighlighted } from '../subcomponents/special_link/links.object';
 
 export default class Callback extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      current: 0,
-      start: 0,
-      end: data.callbackData.length
-    }
-    this.cycle = this.cycle.bind(this);
-    this.reverse = this.reverse.bind(this);
+    // this.state = {
+    //   current: 0,
+    //   start: 0,
+    //   end: data.callbackData.length
+    // }
+    // this.cycle = this.cycle.bind(this);
+    // this.reverse = this.reverse.bind(this);
   }
 
-  cycle() {
-    const { start, end, current } = this.state;
-    let count = current + 1;
+  // cycle() {
+  //   const { current } = this.state;
+  //   let count = current + 1;
 
-    if(count > 4) {
-      count = 0;
-    }
+  //   if(count > 4) {
+  //     count = 0;
+  //   }
     
-    this.setState({
-      current: count
-    });
-  }
+  //   this.setState({
+  //     current: count
+  //   });
+  // }
 
-  reverse() {
-    const { start, end, current } = this.state;
-    let count = current - 1;
+  // reverse() {
+  //   const { current } = this.state;
+  //   let count = current - 1;
 
-    if(count < 0) {
-      count = 4;
-    }
+  //   if(count < 0) {
+  //     count = 4;
+  //   }
     
-    this.setState({
-      current: count
-    });
-  }
-
-  
+  //   this.setState({
+  //     current: count
+  //   });
+  // }
   
   render() {
-
-    const info = data.callbackData;
-    const current = info[this.state.current];
-    const next = "Next";
-    const seehow = "See How";
-    console.log(this.state.current);
+    // const info = data.callbackData;
+    // const current = info[this.state.current];
+    // console.log(this.state.current);
 
     return (
-      <div className={`callback callback__${current.shortName}`}>
-
-        <div className="callback__lefthalfgrid">
-          <div className={`callback__icon callback__icon__${current.shortName}`}/>
+      <div className={'callback callback'}>
+        <div className='callback__left'>
+          <div className='callback__title'>Oops!</div>
+          <div className='callback__subtitle'>
+            Looks like either your license has been expired<br />
+            or you have no permission to access this data.<br />
+            <br />
+            Please reach out to NEXT by submitting a ticket !
+          </div>
         </div>
 
-        <div className="callback__righthalfgrid">
-          <div className="callback__title">
-            <p>Oops!</p>
-          </div>
-          <div className="callback__description">
-            <p>Looks like either your license has been expired or you have no permission to access this data.</p>
-            <p>Please reach out to NEXT by submitting a ticket!</p>
-          </div>
-          <div className="callback__ticket">
-            <p>Ticket</p>
-          </div>
+        <div className='callback__right'>
+          <div className='callback__emoticon'></div>
+          <div className='callback__ticketButtonText'>Ticket</div>
+          <div className='callback__ticketButtonImg'></div>
         </div>
       </div>
+      // <div className={`callback callback__${current.shortName}`}>
+
+      //   <div className="callback__lefthalfgrid">
+      //     <div className={`callback__icon callback__icon__${current.shortName}`}/>
+      //   </div>
+
+      //   <div className="login__righthalfgrid">
+      //     <div className="login__title">
+      //       <p>Welcome!</p>
+      //     </div>
+      //     <div className="login__description">
+      //       <p>Enter <ExternalLinkHighlighted className="about__seehow" value={"hlw"} destination={"https://www.hlw.design/"} /> credentials to grant access to your data!</p>
+      //     </div>
+      //     <div className="login__login">
+      //       <p>Log In</p>
+      //     </div>
+      //     <div className="login__ticket">
+      //       <p>Ticket</p>
+      //     </div>
+      //   </div>
+      // </div>
     )
   }
 }
