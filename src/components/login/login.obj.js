6035ef2ca90e5b1633e3e1c3e89ac11de1fdc30a
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2022-06-15 00:09:14 -0400

    login page stylized

--- src/components/login/login.obj.js
+++ src/components/login/login.obj.js
@@ -7,71 +7,23 @@ import { ExternalLinkHighlighted } from '../subcomponents/special_link/links.obj
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   current: 0,
-    //   start: 0,
-    //   end: data.callbackData.length
-    // }
-    // this.cycle = this.cycle.bind(this);
-    // this.reverse = this.reverse.bind(this);
   }
-
-  // cycle() {
-  //   const { current } = this.state;
-  //   let count = current + 1;
-
-  //   if(count > 4) {
-  //     count = 0;
-  //   }
-    
-  //   this.setState({
-  //     current: count
-  //   });
-  // }
-
-  // reverse() {
-  //   const { current } = this.state;
-  //   let count = current - 1;
-
-  //   if(count < 0) {
-  //     count = 4;
-  //   }
-    
-  //   this.setState({
-  //     current: count
-  //   });
-  // }
-  
   render() {
-    // const info = data.callbackData;
-    // const current = info[this.state.current];
-    // console.log(this.state.current);
 
     return (
       <div className={'login login'}>
-        log the fuck in
+        <div className='NEXTlogo'></div>
+        <div className='spreadsheet'>
+          <div className='text'>
+            Enter hlw Credentials to grant <br />
+            access to your data ! <br />
+            <br />
+            Click the icon below to log in  <br />
+            from Autodesk portal.
+          </div>
+          <div className='lock'></div>
+        </div>
       </div>
-      // <div className={`callback callback__${current.shortName}`}>
-
-      //   <div className="callback__lefthalfgrid">
-      //     <div className={`callback__icon callback__icon__${current.shortName}`}/>
-      //   </div>
-
-      //   <div className="login__righthalfgrid">
-      //     <div className="login__title">
-      //       <p>Welcome!</p>
-      //     </div>
-      //     <div className="login__description">
-      //       <p>Enter <ExternalLinkHighlighted className="about__seehow" value={"hlw"} destination={"https://www.hlw.design/"} /> credentials to grant access to your data!</p>
-      //     </div>
-      //     <div className="login__login">
-      //       <p>Log In</p>
-      //     </div>
-      //     <div className="login__ticket">
-      //       <p>Ticket</p>
-      //     </div>
-      //   </div>
-      // </div>
     )
   }
 }
