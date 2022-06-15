6a1ecf0db48f492f92a527449636b191ed933ce3
Chenzhang Wang <51490717+czwangxtt@users.noreply.github.com>
2022-06-14 21:39:04 -0400

    Clean up the callback.obj.js.

--- src/components/callback/callback.obj.js
+++ src/components/callback/callback.obj.js
@@ -2,34 +2,12 @@ import React from 'react';
 import './callback.style.desktop.scss';
 import './callback.style.mobile.scss';
 import data from '../../data/data.callback.json';
-import { InternalLinkHighlighted } from '../subcomponents/special_link/links.object';
-import {
-  BrowserRouter as Router,
-  Link,
-  useLocation
-} from "react-router-dom";
-import { faSwatchbook } from '@fortawesome/free-solid-svg-icons';
-
-// A custom hook that builds on useLocation to parse the query string.
-function useQuery() {
-  const { search } = useLocation();
-
-  return React.useMemo(() => new URLSearchParams(search), [search]);
-}
-
-function QueryParamsDemo() {
-  let query = useQuery();
-  const code = query.get("code");
-  console.log(code);
-}
 
 export default class Callback extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       current: 0,
-      start: 0,
-      end: data.callbackData.length
     }
     this.updateStatus = this.updateStatus.bind(this);
 
@@ -38,33 +16,6 @@ export default class Callback extends React.Component {
     this.contact = "If you have any question, Please reach out to NEXT by submitting a ticket!";
   }
   
-
-  cycle() {
-    const { start, end, current } = this.state;
-    let count = current + 1;
-
-    if(count > 4) {
-      count = 0;
-    }
-    
-    this.setState({
-      current: count
-    });
-  }
-
-  reverse() {
-    const { start, end, current } = this.state;
-    let count = current - 1;
-
-    if(count < 0) {
-      count = 4;
-    }
-    
-    this.setState({
-      current: count
-    });
-  }
-
   updateStatus(){
     const queryParams = new URLSearchParams(window.location.search);
     const term = queryParams.get("code");
@@ -81,8 +32,6 @@ export default class Callback extends React.Component {
 
     const info = data.callbackData;
     const current = info[this.state.current];
-    const next = "Next";
-    const seehow = "See How";
     
     this.updateStatus();
 
