472596e1fdaf74da37f28ee6e3471f39c7a7530b
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2022-06-22 12:19:36 -0400

    success pop up css restructured

--- src/components/callback/callback.obj.js
+++ src/components/callback/callback.obj.js
@@ -32,17 +32,12 @@ export default class Callback extends React.Component {
 
     return (
       <div className={`callback callback__${current.status}`}>
-        <div className='callback__left'>
-          <div className='callback__title'>{current.Title}</div>
-          <div className='callback__subtitle'>{current.description}</div>
-          <div className='callback__contact'>{current.contact}</div>
-        </div>
-
-        <div className='callback__right'>
-          <div className={`callback__emoticon__${current.status}`}></div>
-          <div className={`callback__ticketButtonText__${current.status}`}>Ticket</div>
-          <a className={`callback__ticketButtonImg__${current.status}`} href='https://hlw.atlassian.net/servicedesk/customer/portal/2'></a>
-        </div>
+        <div className='callback__title'>{current.Title}</div>
+        <div className='callback__subtitle'>{current.description}</div>
+        <div className='callback__contact'>{current.contact}</div>
+        <div className={`callback__emoticon__${current.status}`}></div>
+        <div className={`callback__ticketButtonText__${current.status}`}>Ticket</div>
+        <a className={`callback__ticketButtonImg__${current.status}`} href='https://hlw.atlassian.net/servicedesk/customer/portal/2'></a>
       </div>
     )
   }
